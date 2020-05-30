821971107abbef276122426143df8cb94d831da0
Stephanie <Stephanie@Office-iMac.local>
2020-05-30 16:41:45 -0700

    added summary description and snowy owl

--- src/NavHome/HomePage.js
+++ src/NavHome/HomePage.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
-import './Home.css';
-import '../Font/Font.css';
-import uYtitle from '../Images/uYtitle.png';
-import SearchForm from '../Search/SearchForm';
-import AddTripForm from '../AddTrip/AddTripForm';
-import { NavLink} from 'react-router-dom'
-
+import React, { Component } from "react";
+import "./Home.css";
+import "../Font/Font.css";
+import uYtitle from "../Images/uYtitle.png";
+import snowyOwl from "../Images/snowyOwl.GIF";
+import SearchForm from "../Search/SearchForm";
+import AddTripForm from "../AddTrip/AddTripForm";
+import { NavLink } from "react-router-dom";
 
 class HomePage extends Component {
   render() {
+    const handleResults = this.props.handleResults;
 
-    const handleResults = this.props.handleResults
-
-    console.log(handleResults)
+    console.log(handleResults);
 
     return (
-    <div>
-      <div className= "home">
-        <img className= "uYtitle" src={uYtitle} alt= "ultraYumm logo"/>
-        <h1  className= "black montebello title">Get more energy!<span>&nbsp;&nbsp;</span><span className= "lighter">pack lighter</span></h1> 
+      <div>
+        <div className="home">
+          <img className="uYtitle" src={uYtitle} alt="ultraYumm logo" />
+          <h1 className="black montebello title">
+            Get more energy!<span>&nbsp;&nbsp;</span>
+            <span className="lighter">pack lighter</span>
+          </h1>
+          <div className= "primaryFont description">
+            <div className= "descriptionText"><strong>ultraYumm</strong> helps you plan and pack just the right amount of food for your trip. Search you favorite eats and allocate them in precise amounts to your travelers by day and by meal. Keep everyone energized and happy with ultra-light packs!</div>
+             <div className="snowyOwlcontainer"><img className="snowyOwl" src={snowyOwl} alt="ultraYumm logo" /></div>
+          </div>
+          
+        
+        </div>
 
-       </div>
-       
-       
-      <SearchForm
-      routerProps = {this.props.routerProps}
-      handleUpdate={this.props.handleUpdate}
-      handleResults={this.props.handleResults}
-      ></SearchForm>
+        <SearchForm
+          routerProps={this.props.routerProps}
+          handleUpdate={this.props.handleUpdate}
+          handleResults={this.props.handleResults}
+        ></SearchForm>
 
-    
-            
-      <AddTripForm></AddTripForm>
-   </div>
-   
-   
-   );
+        <AddTripForm></AddTripForm>
+      </div>
+    );
   }
 }
 export default HomePage;
