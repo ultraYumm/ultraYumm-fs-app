cce9a3b9b50f9a91e7c11cc5461b4cdfde7c73ce
Stephanie <Stephanie@Office-iMac.local>
2020-07-07 21:00:30 -0700

    modified build script for Amplify deploy

---
+++ build/precache-manifest.e47c0e75e0f5b7bb1e7ac7bce0b7c7d5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d14ec2c141910d2320225907be7ca7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0296294f7fcc0f71cb01",
+    "url": "/static/css/main.0762d380.chunk.css"
+  },
+  {
+    "revision": "eff9728bc3a697c8369d",
+    "url": "/static/js/2.c82e8685.chunk.js"
+  },
+  {
+    "revision": "e293c143e177a32373554d6ca4c08bba",
+    "url": "/static/js/2.c82e8685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0296294f7fcc0f71cb01",
+    "url": "/static/js/main.bcb51ac8.chunk.js"
+  },
+  {
+    "revision": "0e0083be5e36dd00e826",
+    "url": "/static/js/runtime-main.3a1e3059.js"
+  },
+  {
+    "revision": "9953981ba864301661774b3f66c69641",
+    "url": "/static/media/Montebello-Sans.9953981b.ttf"
+  },
+  {
+    "revision": "0be07412a1c196e7cf302e54d02fea48",
+    "url": "/static/media/NutritionixAPI.c48f1b22.png"
+  },
+  {
+    "revision": "46c28c4db31a229bb5c8c0cf917689bd",
+    "url": "/static/media/snowyOwl.46c28c4d.GIF"
+  },
+  {
+    "revision": "b12ba222e94aff1aaf023c4a0b2199a3",
+    "url": "/static/media/uYhomeLogo.4137a3cb.png"
+  },
+  {
+    "revision": "b2e87a21f57628140fbd8e42f3b3ee49",
+    "url": "/static/media/uYtitle.528791ea.png"
+  }
+]);
