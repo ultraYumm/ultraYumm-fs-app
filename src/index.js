b67d457da3563595708b3ac2bc8fa33477d12645
Stephanie <Stephanie@Office-iMac.local>
2020-07-28 23:18:47 -0700

    removed amplify import in index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './index.css';
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
 
 
 
