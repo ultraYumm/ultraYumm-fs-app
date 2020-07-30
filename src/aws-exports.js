de0be579c7ce754655216cd0c28b12fb162398ca
Shinichi Urano <s@urano.me>
2020-07-30 10:47:07 -0400

    fix packages

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_user_pools_id": "us-west-2_0Vt3rC3cJ",
     "aws_user_pools_web_client_id": "2r03khbbk1k1sp32tdedlnjgj2",
     "oauth": {},
-    "aws_appsync_graphqlEndpoint": ""
+    "aws_appsync_graphqlEndpoint": "",
     "aws_appsync_region": "us-west-2",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
 };
