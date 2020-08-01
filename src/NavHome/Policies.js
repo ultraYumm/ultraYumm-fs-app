391babb8314d63761eed04831ede3d408cab1bba
Stephanie <Stephanie@Office-iMac.local>
2020-08-01 09:52:11 -0700

    fixed email mailto error

--- src/NavHome/Policies.js
+++ src/NavHome/Policies.js
@@ -18,9 +18,9 @@ class Privacy extends Component {
       <img  src={CoreValuesImg} alt="ultraYumm core values" className= "coreValues"/>
       <li className = "hello">
       My name is Stephanie and I coded ultraYumm<span className ="tiny">©</span> after planning a Grand Canyon backpacking trip with my family along the wondrous <a href="https://www.grandcanyontrust.org/hikes/escalante-route" className = "noDeco" alt="url to escalante" target= "_blank" rel="noopener noreferrer">Escalante&nbsp;</a>route. It seemed like there were no good tools to search for foods and plan for maximum energy and minimum carried weight. Fast forward, you are now visiting site version 1.0! <br></br> <br></br>
-      Full disclosure, this is a prototype, homemade with love, that I'm working on in my spare time. I also still have much to learn about web development. Please send me your  <a href="mailto:mailto:ultrayummprod@gmail.com?subject= Please share your feedback, questions, anything!" className = "noDeco" alt="subject" target= "_blank" rel="noopener noreferrer" >comments&nbsp;</a>or post a review on <span>
+      Full disclosure, this is a prototype, homemade with love, that I'm working on in my spare time. I also still have much to learn about web development. Please send me your  <a href="mailto:ultrayummprod@gmail.com?subject= Please share your feedback, questions, anything!" className = "noDeco" alt="email" target= "_blank" rel="noopener noreferrer" title="Email your feedback">comments&nbsp;</a>or post a review on <span>
       <a href="https://www.facebook.com/ultraYumm" className = "noDeco" alt="subject" target= "_blank" rel="noopener noreferrer"> <i className="fab fa-facebook" alt= "facebook page" title="Email your feedback"></i></a>
-</span> thank you and happy trails!
+</span> <br></br>Thank you and happy trails!
         </li>   
 
         <li>
